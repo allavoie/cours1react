84809dc0ada15709a8d3eac193ae4e620eab397a
Alain <alain.lavoie@dexto.ca>
2018-01-27 13:34:21 -0500

    ok

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+const db = mongoose.connection;
 
 const app = express();
-passport.use(new GoogleStrategy());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [ keys.cookieKey ],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-const APORT = process.env.PORT || 5000;
-app.listen(APORT);
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
